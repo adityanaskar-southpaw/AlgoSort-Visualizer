23a038998f5ec6d22d5bf31d57f88ba59f9b2441
Aditya Naskar <adityanaskar2612@gmail.com>
2026-04-15 18:09:01 +0530

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,25 +1,13 @@
 import random
-
-# -----------------------------
 # RANDOM DATA
-# -----------------------------
 def generate_random(n):
     return [random.randint(1, 1000) for _ in range(n)]
-
-# -----------------------------
 # SORTED DATA (BEST CASE)
-# -----------------------------
 def generate_sorted(n):
     return list(range(n))
-
-# -----------------------------
 # REVERSED DATA (WORST CASE)
-# -----------------------------
 def generate_reversed(n):
     return list(range(n, 0, -1))
-
-# -----------------------------
-# DUPLICATE HEAVY DATA (OPTIONAL)
-# -----------------------------
+# DUPLICATE HEAVY DATA 
 def generate_duplicates(n):
     return [random.choice([10, 20, 30, 40, 50]) for _ in range(n)]
